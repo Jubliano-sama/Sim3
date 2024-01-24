b00598d8dbd9282c416d20078b095d87c8e5ad2b
TimVerbeek05 <t.verbeek@student.fontys.nl>
2024-01-24 12:18:33 +0100

    calibration

--- src/basicFunctions.h
+++ src/basicFunctions.h
@@ -3,15 +3,14 @@
 // 0 = No Debug, 1 = Only special cases(pause,stop,overshoot), 2 = everyhting
 #define DEBUG 2
 
-const double Kp = 0.11;
+const double Kp = 0.12;
 const double Ki = 0.000002;
-const double Kd = 0;
-
+const double Kd = -2;
 const int switchPin = 53;
 
-const int analogSensors[] = {A0, A1, A2, A3, A4, A5, A7, A8, A9}; // Update with actual analog sensor pins
+const int analogSensors[] = {A0, A1, A2, A3, A5, A4, A7, A8, A9}; // Update with actual analog sensor pins
 const int analogSensorsCount = 9; // Update with the actual number of analog sensors
-const int analogSensorThresholds[] = {150, 280, 150, 150, 150, 150, 150, 150, 150}; // Update with actual threshold values
+const int analogSensorThresholds[] = {150, 150, 150, 150, 150, 150, 150, 150, 150}; // Update with actual threshold values
 
 // Add array index to this to disable them as input
 const int disabledSensors[] = {};
@@ -20,7 +19,7 @@ const int disabledSensorsCount = 0;
 const int motorsForwardPins[]={51,49,47,45}; //{lv,la,rv,ra}
 const int motorsBackwardPins[]={50,48,46,44};
 const int motorsENAPins[]={6,3,4,5};
-const int stopPauseDelay = 1000;
+const int stopPauseDelay = 700;
 
 extern int lastDirection;
 
